e737c8bb95388807f4fca91c94ee09ed1601b91d
Mihir Wankhade <abhiwankhaded67@gmail.com>
2023-04-06 23:42:38 +0530

    final commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => { 
+    return (
+        <div>
+            This is a Medication Reminder App build with React.js by Mihir Wankhade
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/AddNote.js
@@ -0,0 +1,120 @@
+import React, { useContext, useState } from "react";
+import noteContext from "../context/notes/noteContext";
+
+const AddNote = () => {
+  const context = useContext(noteContext);
+  const { addNote } = context;
+
+  const [note, setNote] = useState({ title: "", description: "", tag: "" });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    addNote(note.title, note.description, note.tag);
+    setNote({ title: "", description: "", tag: "" });
+  };
+
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
+  return (
+    <div className="container my-3">
+      <div>
+        <div
+          id="carouselExampleAutoplaying"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="images/fit.jpg" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="images/hg3.jpg" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="images/fit2.jpg" className="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <h2 className="my-3">Add a Reminder</h2>
+      <form className="my-3">
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Medicine Reminder
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            aria-describedby="emailHelp"
+            value={note.title}
+            onChange={onChange}
+            minLength={5}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Medicine Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            name="description"
+            value={note.description}
+            onChange={onChange}
+            minLength={5}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="tag" className="form-label">
+            Time
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="tag"
+            name="tag"
+            value={note.tag}
+            onChange={onChange}
+            minLength={5}
+            required
+          />
+        </div>
+
+        <button
+          disabled={note.title.length < 5 || note.description.length < 5}
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleClick}
+        >
+          Add Note
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddNote;
